2a43cb032a5e0e7067fe0c71902ffb3545895029
Dominik Bartsch <dominik.bartsch@gmx.net>
2023-06-25 12:37:59 +0200

    Add Cell canvas with variable size and print it

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+
 use std::fmt;
 
-pub struct Cells(Vec<CellState>);
+/// a Cell canvas
+#[derive(Debug)]
+pub struct Cells(Vec<Vec<CellState>>);
 
 impl Cells {
-    
+    /// create a new Cell canvas
+    pub fn new(width: usize, height: usize) -> Self {
+        Self { 0: vec![vec![CellState::Dead; width]; height] }
+    }
+}
+
+impl fmt::Display for Cells {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in &self.0 {
+            for cell in row {
+                write!(f, "{cell}")?;
+            }
+            write!(f,"\n")?;
+        }
+        write!(f, "")
+    }
 }
 
 /// State of a Cell
+#[derive(Clone, Debug)]
 enum CellState {
     Alive,
     Dead
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+/// # game_of_life
+/// another rust implementation of the Game of Live
+
 use game_of_life::Cells;
 
 fn main() {
-    
+    let cells = Cells::new(40, 20);
+    println!("{cells}");
 }
